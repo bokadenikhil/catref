8b4eb186ea5a6bbad5e0dd718f1f8352e1ff6abf
Nikhil Bokade <68290121+bokadenikhil@users.noreply.github.com>
2023-04-17 13:54:27 +0530

    Update app.py

--- code/app.py
+++ code/app.py
@@ -6,17 +6,19 @@ import matplotlib.pyplot as plt
 import joblib
 import os
 #------------------#
-path = r'/app/catref/src'
-model_path = r'/app/catref/model'
-model_filename = r'/model.sav'
+current_path = os.getcwd()
+st.write(os.listdir())
+#path = r'/app/catref/src'
+#model_path = r'/app/catref/model'
+#model_filename = r'/model.sav'
 #---------------------------------#
-try:
-    rf = joblib.load(model_path+model_filename)
-except:
-    st.write(os.getcwd())
+#try:
+ #   rf = joblib.load(model_path+model_filename)
+#except:
+ #   st.write(os.getcwd())
 #im = Image.open(path+'\catalytic-reformer.jpg')
 with st.sidebar:
-    st.markdown('#Catalytic Reformer')
-    st.image(im,width=300)
+    st.markdown('Catalytic Reformer')
+   # st.image(im,width=300)
     start = st.date_input('Start Date')
     end = st.date_input('End Date')
