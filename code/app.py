58d2f62fd825a5b0ed952f1bcc5f591e01b79498
Nikhil Bokade <nikhil.bokade@TSPL.LOCAL>
2023-04-17 11:42:23 +0530

    files added

---
+++ code/app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+from streamlit_extras.app_logo import add_logo
+from PIL import Image
+import pandas as pd
+import openpyxl
+import matplotlib.pyplot as plt
+import pickle
+#------------------#
+path = r'..\src'
+model_path = r'..\model'
+model_filename = r'\model.pkl'
+#---------------------------------#
+with open(model_path+model_filename,'rb') as file:
+    rf = pickle.load(file)
+im = Image.open(path+'\catalytic-reformer.jpg')
+with st.sidebar:
+    st.markdown('#Catalytic Reformer')
+    st.image(im,width=300)
+    start = st.date_input('Start Date')
+    end = st.date_input('End Date')
